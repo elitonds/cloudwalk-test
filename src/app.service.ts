60cb3206b879d9dc72f7bcb213fc7da92e94bc56
elitonds <elitonds@gmail.com>
2022-05-12 09:35:54 -0300

    grouped information by game

--- src/app.service.ts
+++ src/app.service.ts
@@ -17,33 +17,34 @@ export class AppService {
     const gameDetailList = new Map<string, GameDetailResults>();
     const playerDetail = new Map<string, Map<string, number>>();
     let gameNumber = 0;
-    let currentGame = '';
+    let currentGameName = '';
 
     for await (const line of rl) {
       if (line.includes('InitGame')) {
-        if (gameDetailList.size && gameDetailList.get(currentGame).totalKills) {
-          gameDetailList.get(currentGame).kills = [
-            ...playerDetail.get(currentGame).entries(),
-          ];
+        const currentGame = gameDetailList.get(currentGameName);
+        const currentPlayerDetail = playerDetail.get(currentGameName);
+        if (gameDetailList.size && currentGame.totalKills) {
+          currentGame.kills = [...currentPlayerDetail.entries()];
+          currentGame.players = [...currentPlayerDetail.keys()];
         }
         gameNumber++;
-        currentGame = `game_${gameNumber}`;
+        currentGameName = `game_${gameNumber}`;
         const gameDetail: GameDetailResults = {
           kills: [],
           players: [],
           totalKills: 0,
         };
-        gameDetailList.set(currentGame, gameDetail);
-        playerDetail.set(currentGame, new Map());
+        gameDetailList.set(currentGameName, gameDetail);
+        playerDetail.set(currentGameName, new Map());
       } else {
         this.processLine(
           line,
-          gameDetailList.get(currentGame),
-          playerDetail.get(currentGame),
+          gameDetailList.get(currentGameName),
+          playerDetail.get(currentGameName),
         );
       }
     }
-    return [...gameDetailList.entries()];
+    return { gameInfo: [...gameDetailList.entries()] };
   }
 
   private processLine(
@@ -57,20 +58,49 @@ export class AppService {
       const killInfo = slicesLine.find((a) => a.includes(' killed '));
       if (killInfo) {
         const detailsKill = killInfo.split(' killed ') || [];
-        detailsKill.forEach((detail) => {
-          const cleanDetail = detail.trim();
-          if (cleanDetail !== '<world>') {
-            if (cleanDetail.includes(' by ')) {
-              const detailsBy = cleanDetail.split(' by ') || [];
-              if (detailsBy.length) this.setPlayer(detailsBy[0], playerDetail);
-            } else this.setPlayer(cleanDetail, playerDetail);
+
+        if (detailsKill.length) {
+          const killerPlayer = detailsKill[0].trim();
+          const killedPlayerData = detailsKill[1].trim();
+          let killedPlayer = '';
+          let weaponInfo = '';
+          let killedByWorld = false;
+
+          if (killerPlayer === '<world>') {
+            killedByWorld = true;
+          } else {
+            this.setPlayer(killerPlayer, playerDetail, true);
           }
-        });
+
+          if (killedPlayerData.includes(' by ')) {
+            const detailsBy = killedPlayerData.split(' by ') || [];
+            if (detailsBy.length) {
+              killedPlayer = detailsBy[0];
+              weaponInfo = detailsBy[1].trim();
+              this.setPlayer(killedPlayer, playerDetail, false, killedByWorld);
+            }
+          }
+        }
       }
     }
   }
 
-  private setPlayer(playerName: string, kills: Map<string, number>) {
-    if (!kills.get(playerName)) kills.set(playerName, 0);
+  private setPlayer(
+    playerName: string,
+    kills: Map<string, number>,
+    isKiller = false,
+    killedByWorld = false,
+  ) {
+    const playerkills = kills.get(playerName);
+    if (playerkills === undefined) kills.set(playerName, isKiller ? 1 : 0);
+    else {
+      if (isKiller) {
+        kills.set(playerName, playerkills + 1);
+      } else {
+        if (killedByWorld) {
+          kills.set(playerName, playerkills - 1);
+        }
+      }
+    }
   }
 }
